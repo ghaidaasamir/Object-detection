e6619ff1d42f5ce0a468f128d70b163fa4503606
ghaidaasamir <107316343+ghaidaasamir@users.noreply.github.com>
2022-10-06 00:24:00 +0200

    Update test.py

--- src/test.py
+++ src/test.py
@@ -143,7 +143,8 @@ if __name__ == '__main__':
 
             print('\tDone testing the {}th sample, time: {:.1f}ms, speed {:.2f}FPS'.format(batch_idx, (t2 - t1) * 1000,
                                                                                            1 / (t2 - t1)))
-            cv2.imwrite(out_img)
+            
+            cv2.imwrite('1',out_img)
             print(configs.results_dir)
             cv2.imshow('image',out_img)
             
