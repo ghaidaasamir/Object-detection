8f482de9a146e8c38e37fe055512b584c1814f9d
ghaidaasamir <107316343+ghaidaasamir@users.noreply.github.com>
2022-10-06 00:07:22 +0200

    Update test.py

--- src/test.py
+++ src/test.py
@@ -143,7 +143,11 @@ if __name__ == '__main__':
 
             print('\tDone testing the {}th sample, time: {:.1f}ms, speed {:.2f}FPS'.format(batch_idx, (t2 - t1) * 1000,
                                                                                            1 / (t2 - t1)))
-
+            cv2.imshow('image',out_img)
+            print(configs.results_dir)
+            img_fn = os.path.basename(img_paths[0])[:-4]
+            cv2.imwrite(os.path.join(configs.results_dir, '{}.jpg'.format(img_fn)), out_img)
+            cv2.imwrite(os.path.join('kaggle/working/', '{}.jpg'.format(img_fn)), out_img)
             if configs.save_test_output:
                 if configs.output_format == 'image':
                     img_fn = os.path.basename(img_paths[0])[:-4]
